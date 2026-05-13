0dcdbbf59879eee1602340dd2d16f3e86cb28a3e
Bobbie Soedirgo <bobbie@soedirgo.dev>
2026-05-13 23:16:10 +0700

    chore: bump image defaults
    Bump to sha-9d84da5 to ensure pgctld has POSTGRES_INITDB_EXTRA_CONF
    support
    
    Signed-off-by: Bobbie Soedirgo <bobbie@soedirgo.dev>

--- api/v1alpha1/image_defaults.go
+++ api/v1alpha1/image_defaults.go
@@ -5,25 +5,25 @@ package v1alpha1
 const (
 	// DefaultPostgresImage is the default container image for PostgreSQL instances.
 	// Uses the pgctld image which bundles PostgreSQL, pgctld, and pgbackrest.
-	DefaultPostgresImage = "ghcr.io/multigres/pgctld:sha-5b997b4"
+	DefaultPostgresImage = "ghcr.io/multigres/pgctld:sha-9d84da5"
 
 	// DefaultEtcdImage is the default container image for the managed Etcd cluster.
 	DefaultEtcdImage = "gcr.io/etcd-development/etcd:v3.6.7"
 
 	// DefaultMultiAdminImage is the default container image for the MultiAdmin component.
-	DefaultMultiAdminImage = "ghcr.io/multigres/multigres:sha-5b997b4"
+	DefaultMultiAdminImage = "ghcr.io/multigres/multigres:sha-9d84da5"
 
 	// DefaultMultiAdminWebImage is the default container image for the MultiAdminWeb component.
-	DefaultMultiAdminWebImage = "ghcr.io/multigres/multiadmin-web:sha-d7be6e4"
+	DefaultMultiAdminWebImage = "ghcr.io/multigres/multiadmin-web:sha-c2db14c"
 
 	// DefaultMultiOrchImage is the default container image for the MultiOrch component.
-	DefaultMultiOrchImage = "ghcr.io/multigres/multigres:sha-5b997b4"
+	DefaultMultiOrchImage = "ghcr.io/multigres/multigres:sha-9d84da5"
 
 	// DefaultMultiPoolerImage is the default container image for the MultiPooler component.
-	DefaultMultiPoolerImage = "ghcr.io/multigres/multigres:sha-5b997b4"
+	DefaultMultiPoolerImage = "ghcr.io/multigres/multigres:sha-9d84da5"
 
 	// DefaultMultiGatewayImage is the default container image for the MultiGateway component.
-	DefaultMultiGatewayImage = "ghcr.io/multigres/multigres:sha-5b997b4"
+	DefaultMultiGatewayImage = "ghcr.io/multigres/multigres:sha-9d84da5"
 
 	// DefaultPostgresExporterImage is the default container image for postgres_exporter sidecars.
 	DefaultPostgresExporterImage = "quay.io/prometheuscommunity/postgres-exporter:v0.18.1"
